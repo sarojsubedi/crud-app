b13593c769aefc77982b2ea860317f6104690959
sarojsubedi <sarojsubedi@pm.me>
2025-10-13 22:07:12 +0545

    first commit

---
+++ index.ts
@@ -0,0 +1,44 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("/users endpoint for CRUD operations");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.send(users);
+});
+
+app.post("/users", async (req, res) => {
+  const user = await prisma.user.create({ data: req.body });
+  res.json(user);
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const deleteUser = await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.send(`User with id:${req.params.id} deleted successfully`);
+});
+
+app.put("/users/:id", async (req, res) => {
+  const updateUser = await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: req.body,
+  });
+  res.send(updateUser);
+});
+
+app.listen(3000, () => {
+  console.log("Server running at https://localhost:3000");
+});
